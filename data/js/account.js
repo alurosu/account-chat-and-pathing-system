979860189c8f8afddca9855c39014b4a84090985
Lucian Adrian Rosu <alurosu@gmail.com>
2018-07-19 19:45:17 +0300

    added login loader

--- data/js/account.js
+++ data/js/account.js
@@ -1,10 +1,39 @@
 $(document).ready(function(){
 	console.log('account.js: loaded');
-	var id = getParameterByName('id');
-	if (id != "") {
-		console.log(id);
-	}
+	var page = getParameterByName('page');
+	if (page != 'register' && page != 'forgot') 
+		page = 'login';
+	
+	$("#"+page).addClass('active');
+	
+	// login
+	$('#login').on('click', 'button', function(){
+		if (!$(this).hasClass('loading')) {
+			showBtnLoader($(this));
+		} else hideBtnLoader($(this));
+	});
+	
+	// register
+	$('#register').on('click', 'button', function(){
+		if (!$(this).hasClass('loading')) {
+			showBtnLoader($(this));
+		} else hideBtnLoader($(this));
+	});
 });
+
+function showBtnLoader(btn) {
+	btn
+		.attr('data-html',btn.html())
+		.html('')
+		.addClass('loading')
+		.parent().find('input').prop('disabled', true);
+}
+function hideBtnLoader(btn) {
+	btn
+		.html(btn.attr('data-html'))
+		.removeClass('loading')
+		.parent().find('input').prop('disabled', false);
+}
 
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
