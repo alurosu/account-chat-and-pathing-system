2f4c2fd7885b0737fd37c9f54ee50254c95545f2
alurosu <alurosu@gmail.com>
2019-08-12 16:48:00 +0300

    admin start

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Admin Panel @ Among Demons</title>
+	<link rel="stylesheet" href="data/main.css" />
+
+	<script type="text/javascript" src="data/jquery.js"></script>
+</head>
+
+<body>
+	div#test
+</body>
+</html>
